ff4a2dc22208669f426c7b6cf9c9422b042f7ffd
diemonBil <118901446+diemonBil@users.noreply.github.com>
2023-02-12 09:52:26 +0200

    Add lab 1

---
+++ lab1/js/main.js
@@ -0,0 +1,11 @@
+
+$( window ).resize(function() {
+    if($( window ).height() < 750){
+        $(".footer").addClass("footer-padding");
+        $(".footer p").addClass("footer-p")
+    }else{
+        $(".footer").removeClass("footer-padding");
+        $(".footer p").removeClass("footer-p")
+    }
+  });
+$(window).trigger("resize");
